65a12302b1a95ffb6b6ac5e31a0367f6973f50da
domjweb <dominiquejadonwebb@gmail.com>
2025-12-13 18:11:00 -0500

    Simple exercises for Python basics

---
+++ python/comments/121325.py
@@ -0,0 +1,6 @@
+# Today is a new day and I am commenting another comment 
+
+'''
+Another multi-string used as a comment for practice purposes and
+routine/habit building
+'''
---
+++ python/print_statements/121325.py
@@ -0,0 +1,11 @@
+# This line prints the word "Hello."
+print("Hello.")
+# This line prints the word "Hello!"
+print("Hello!")
+
+def hello():
+    print("Hello...")
+hello()
+
+'''Not a pure print statement but I did want to include
+as a simple reminder'''
