6b49bed4f58e7523bc97d291cfd843a35edc478e
domjweb <dominiquejadonwebb@gmail.com>
2025-12-15 11:10:52 -0500

    Started today's practice with Python comments and multi-line strings

---
+++ python/comments/121525.py
@@ -0,0 +1,7 @@
+# I am once again practicing single line comments in Python
+
+'''
+I am once again practicing multi-line strings that 
+serve as comments in Python when not used in
+any relevant context
+'''
