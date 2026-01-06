857499312a8bd488a8fce8a6cea9df0eafd134b0
domjweb <dominiquejadonwebb@gmail.com>
2026-01-06 16:40:06 -0500

    Added today's print statements

---
+++ python/print_statements/1626.py
@@ -0,0 +1,8 @@
+# Printing again on the 6th of January
+
+print("Hol up, printing again, HUAAHH")
+
+#Using a function for a print statement 
+def printing():
+    print("Here I go, using a FUNCTION to print again, MMMMM")
+printing()
