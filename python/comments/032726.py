34b60f6a452db3af458f1505848421a74136acdb
Dominique Webb <domjweb@Dominiques-MacBook-Air.local>
2026-03-27 09:22:01 -0400

    Started back daily practices and disabled inline suggestions on new machine

---
+++ python/comments/032726.py
@@ -0,0 +1,8 @@
+# Lol it's been a while since I've done this. The year has been crazier for my development journey than I ever imagined.
+
+'''
+I have been all over the place as a developer this year and I'm more blessed than I could ever imagine.
+This is a nice check in and a point to get back to my daily routine and consistent practice.
+I have a new environment for Mac development and a new computer coming soon to setup for Windows on the 31st.
+ONE DAY I will have a Linux machine to call my own but for now I will be working on both of these environments.
+The auto complete is really throwing me off I need to turn that off. I believe I 
