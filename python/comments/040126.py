2e8addf325ac06c4e7452bdc5a7a0c2f331b4242
domjweb <dominiquejadonwebb@gmail.com>
2026-04-01 19:10:28 -0400

    Added comments to the python repo, may actually do some personal work today

---
+++ python/comments/040126.py
@@ -0,0 +1,16 @@
+# Consistency? APRIL FOOLS! I did this same thing in the JS repo as well in case anyone reads this I'm sorry lol
+
+'''
+
+So I have not had a chance to actually write any code in my personal repos...
+
+I have a LOT of personal work I want to do but that comes second to the almighty dollar.
+
+I WILL make time to get things done this is a promise I am making that is NOT an April Fools joke lol
+
+I'm just not sure it'll be right now XD either way... 
+
+I'll do my best to keep enforcing the habit of committing to my personal github account even if
+it is only a few comments.
+
+'''
