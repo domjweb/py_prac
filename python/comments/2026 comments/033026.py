c934cbf99c131ebd61f87899cd8e0ad9d71cc3fe
domjweb <dominiquejadonwebb@gmail.com>
2026-04-27 01:07:13 -0400

    Modified some files and added a new one, hopefully getting back on track with daily commits

---
+++ python/comments/2026 comments/033026.py
@@ -0,0 +1,11 @@
+# Writing comments to commit to personal repos before getting back into work study
+
+'''
+
+Working on some comments to push to repos before getting back to work studies and then 
+HOPEFULLY
+coming back to actually write some code for my personal repos tonight
+...
+no promises XD
+
+'''
---
+++ python/comments/2026 comments/040226.py
@@ -0,0 +1,11 @@
+# More python comments!
+
+'''
+
+I have work to finish
+
+and I will finish it.
+
+This is the way!
+
+'''
