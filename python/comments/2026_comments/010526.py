a892ea2804f5f52aa463a94a1092636c6743f570
domjweb <dominiquejadonwebb@gmail.com>
2026-07-18 15:55:42 -0400

    Added some comments to the repo and changed the way the directories were set up for ease of use

---
+++ python/comments/2026_comments/010526.py
@@ -0,0 +1,6 @@
+# Practicing my comments to start the new year
+
+'''
+Practicing my multi-line strings that serve as comments 
+to start the new year
+'''
---
+++ python/comments/2026_comments/032926.py
@@ -0,0 +1,7 @@
+# Getting started with comments again and I plan on doing a little more on the new machine today
+
+'''
+I have work to do for the company I work for but I do want to get practice in early this morning.
+I'm not really doing anything TOO heavy this morning. Just getting some comments and maybe writing some
+functions before I get started on other things. I will probably do more practice once I get done with that.
+'''
